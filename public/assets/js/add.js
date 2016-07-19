3b695ca3f18846b0b669eb27ff7ab3282b9bef35
rikutech <rikutech@gmail.com>
2016-07-19 14:11:28 +0900

    不要なコードを削除

--- public/assets/js/add.js
+++ public/assets/js/add.js
@@ -1,47 +1,12 @@
 //$(document).readyで画面読み込みと同時に処理を開始
   $(document).ready(function(){
-    //google maps apiの一連の処理が書かれたinitialize関数を開始
-  google.maps.event.addDomListener(window, 'load', initialize);
-  $('#confirm').attr('disabled', false);
   //edit.php用に、文字数制限のある項目の初期の長さを取得
   $('#title_count').text($('#title').val().length);
   $('#capacity_count').text($('#capacity').val().length);
   $('#detail_count').text($('#detail').val().length);
 
 });
- //google maps apiの一連の処理
- function initialize() {
-       //searchTextFieldを取得し変数に格納
-     var input = document.getElementById('searchTextField');
 
-     var options = {
-       //フィリピン国内のみ表示するよう指定
-         componentRestrictions: {country: 'ph'}
-     };
-
-     var autocomplete = new google.maps.places.Autocomplete(input, options);
-     google.maps.event.addListener(autocomplete, 'place_changed', function () {
-         $('#loading').css('display', 'inline');
-         var place = autocomplete.getPlace();
-         //場所名と緯度経度を取得
-         $('#place_name').attr('value', place.name);
-         $('#lat').attr('value', place.geometry.location.lat());
-         $('#lng').attr('value', place.geometry.location.lng());
-         $('#loading').css('display', 'none');
-         if (  $('#title').val()         !=='' 
-            && $('#date').val()          !=='' 
-            && $('#starting_time').val() !=='' 
-            && $('#detail').val()        !=='') 
-         {
-         $('#confirm').attr('disabled', false);
-            }
-         });
- }
-
- //Placeに何か変更があった場合confirmを無効にする
- $('#searchTextField').keydown(function() {
-     $('#confirm').attr('disabled', true);
- });
 
  //3つある画像のプレビューを共通の関数で処理するためにそれぞれのidを取得する関数
  $('.pic').change(function(){
@@ -76,12 +41,8 @@
 //一つでも空欄ができれば再びdisabledをtrueに
 $('.form-control').change(function() {
     if (       $('#title').val()         !=='' 
-            && $('#date').val()          !=='' 
-            && $('#starting_time').val() !=='' 
-            && $('#detail').val()        !==''
-            && $('#place_name').val()    !==''
-            && $('#lat').val()           !==''
-            && $('#lng').val()           !=='') 
+            && $('#price').val()         !==''
+            && $('#detail').val()        !=='') 
          {
          $('#confirm').attr('disabled', false);
             } else {
