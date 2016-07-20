047e28ef4e71902a4e5a6740e858e5f114f00eb9
松澤 隼人 <hayatomatsuzawa@gmail.com>
2016-07-20 14:14:40 +0900

    文章一旦変更

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,14 +14,15 @@
         <div>
             <div>
 
-                <h1 class="logo-name">IN+</h1>
+                <h1 class="logo-name">+ ヒ ト</h1>
 
             </div>
-            <h3>Welcome to IN+</h3>
-            <p>Perfectly designed and precisely prepared admin theme with over 50 pages with extra new web app views.
+            <h3>Welcome to + ヒト</h3>
+            <p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+            xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.
                 <!--Continually expanded and constantly improved Inspinia Admin Them (IN+)-->
             </p>
-            <p>Login in. To see it in action.</p>
+            <p>xxxxxxxxxxxxxxxxxxxxxxxx.</p>
             <form class="m-t" role="form" action="index.html">
                 <div class="form-group">
                     <input type="email" class="form-control" placeholder="Username" required="">
@@ -31,9 +32,9 @@
                 </div>
                 <button type="submit" class="btn btn-primary block full-width m-b">Login</button>
 
-                <a href="#"><small>Forgot password?</small></a>
-                <p class="text-muted text-center"><small>Do not have an account?</small></p>
-                <a class="btn btn-sm btn-white btn-block" href="register.html">Create an account</a>
+                <a href="#"><small>パスワードをお忘れの場合</small></a>
+                <p class="text-muted text-center"><small>すでにアカウントをお持ちの方</small></p>
+                <a class="btn btn-sm btn-white btn-block" href="register.html">サインアップ</a>
             </form>
             <p class="m-t"> <small>Inspinia we app framework base on Bootstrap 3 &copy; 2014</small> </p>
         </div>
